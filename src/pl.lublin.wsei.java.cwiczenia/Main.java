99d569094138f8e1bfd2eff04440e501f676e5b9
Smilekook <samakhval.vip@gmail.com>
2021-04-14 15:32:26 +0200

    punkt 1-5

--- src/pl.lublin.wsei.java.cwiczenia/Main.java
+++ src/pl.lublin.wsei.java.cwiczenia/Main.java
@@ -1,7 +1,16 @@
 package pl.lublin.wsei.java.cwiczenia;
-
+/* The code below will print the words Hello Java world
+   to the terminal/ console */
 public class Main {
+    /**
+     * Java main method is the entry point of any java program.
+     * Public static void main(String[] args) is always its syntax.
+     * You can only change the name of String array argument, for example
+     * you can change args to BestStringArgs
+     * @param  args          args contains the supplied command-line arguments as an array of String objects
+     * @see                  String
+     */
     public static void main(String[] args){
-        System.out.println("Hello Java world\n");
+        System.out.println("Hello Java world\n");// print the words Hello Java world
     }
 }
