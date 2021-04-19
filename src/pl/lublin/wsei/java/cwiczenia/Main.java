d7045ea1f42b7b755086b492e4b6b77d5b90575d
Smilekook <samakhval.vip@gmail.com>
2021-04-19 14:18:53 +0200

    punkt 12

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,8 @@
+package pl.lublin.wsei.java.cwiczenia;
+import pl.lublin.wsei.java.cwiczenia.mylib.Account;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("%s%n",Account.translit("Самохвал"));
+    }}
+
---
+++ src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -0,0 +1,22 @@
+package pl.lublin.wsei.java.cwiczenia.mylib;
+import org.apache.commons.lang3.StringUtils;
+
+
+public class Account {
+    private static String ukrAlphabet = "абвгдеєжзиіїйклмнопрстуфхцчшщьюя";
+    private static String translitRepl[] = {"a","b","v","g","d","e","je","zh","z","y","i","ji", "j",
+            "k","l","m","n","o","p","r","s","t","u","f","h","c",
+            "ch","sh","shh","'","ju","ja"};
+
+    public static String translit (String arg){
+        StringBuilder trans = new StringBuilder();
+        arg = arg.toLowerCase();
+        for(int i = 0; i < arg.length(); i++){
+            trans.append(ukrAlphabet.indexOf(arg.charAt(i)) >= 0 ? translitRepl[ukrAlphabet.indexOf(arg.charAt(i))] : arg.charAt(i));
+        }
+        return trans.toString();
+
+
+    }
+
+}
