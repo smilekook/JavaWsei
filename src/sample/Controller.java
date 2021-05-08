108b1a46e7946e37129d3d908c372bd2e6c027ff
Smilekook <samakhval.vip@gmail.com>
2021-05-08 19:14:06 +0200

    punkt 6

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,32 @@
 package sample;
-
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class Controller {
     public Button btnMain;
-    public TextField txtLicznik;
-    int clickCounter = 0;
+    public TextField txtLicznik1;
+    public TextField txtLicznik2;
+    public TextField txtLicznik3;
+    public TextField txtLicznik4;
+    public TextField txtLicznik5;
+    public TextField txtLicznik6;
+    public TextField txtLicznik7;
+    double a, b, c;
+    int cenR = 3;
+    int cenaNieR = 13;
+    double cR = 0.7;
+    double cNieR = 1.2;
+    public void onBtnAction(ActionEvent actionEvent){
+         a = 2 * 3.14 * (Integer.parseInt(txtLicznik1.getText()) + Integer.parseInt(txtLicznik2.getText()));
+         b = 2 * 3.14 * (Integer.parseInt(txtLicznik1.getText()) - Integer.parseInt(txtLicznik3.getText()) +
+                 Integer.parseInt(txtLicznik2.getText()));
+         c = a - b;
+         txtLicznik4.setText(c * cenR + "");
+         txtLicznik6.setText(c * cenaNieR + "");
+         txtLicznik5.setText(c * cR + "");
+         txtLicznik7.setText(c * cNieR +"");
 
-    public void onBtnAction(ActionEvent actionEvent) {
-       txtLicznik.setText("Kliknięcie nr" + ++clickCounter);
-        if (clickCounter == 1) btnMain.setText("i jeszcze raz");
+
     }
 }
